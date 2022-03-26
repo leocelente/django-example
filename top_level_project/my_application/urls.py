cd76d0a7f41e2eae31e0ae861d41aa00ad11b778
Leonardo Celente <leocelente@gmail.com>
2022-03-26 15:06:30 -0300

    Added Slide page

--- top_level_project/my_application/urls.py
+++ top_level_project/my_application/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_page, name='index'),
+    path('slide/<int:slide_id>/', views.slide_page, name='slide')
 ]
--- top_level_project/my_application/views.py
+++ top_level_project/my_application/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index_page(request):
     data = { "i" : 1}
     return render(request, "my_application/home.html", data)
+
+def slide_page(request, **kwargs):
+    gambiarra = [ "https://imgur.com/GiA65YF", "https://imgur.com/5BqsYrq", "https://imgur.com/ymb6hvT", "https://imgur.com/2AasabR"]
+    i = kwargs["slide_id"] % len(gambiarra)
+    data = { "i": i, "next": i+1, "prev": i-1,  "url": gambiarra[i]}
+    return render(request, "my_application/slide.html", data)
