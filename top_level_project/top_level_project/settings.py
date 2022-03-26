74dd6d9633099d2acaaa9554e2f08fcb803beb77
Leonardo Celente <leocelente@gmail.com>
2022-03-26 12:18:02 -0300

    Added static files config to settings.py

--- top_level_project/top_level_project/settings.py
+++ top_level_project/top_level_project/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / "my_application/static"]  
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
